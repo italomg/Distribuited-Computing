0cfb8337cdb8623edcfb51bb00aeb51877d0db0a
Fernando <nando_hlb@yahoo.com.br>
2017-07-08 12:17:40 -0300

    Ajustestes run_measurements

--- run_measurements.sh
+++ run_measurements.sh
@@ -2,30 +2,40 @@
 
 set -o xtrace
 
-MEASUREMENTS=10
-ITERATIONS_THREAD=2
-ITERATIONS_SIZE=2
-INITIAL_THREADS=$(nproc)
-IMG_SIZE=512
+MEASUREMENTS=2
+SIZE=8192
+INSTANCES=localhost
+THREADS=$(nproc)
+#ITERATIONS_THREAD=2
+#ITERATIONS_SIZE=2
+#INITIAL_THREADS=$(nproc)
+#IMG_SIZE=512
 
 make
 
 #MANDELBROT: Sequential printing only algorithm time
-mkdir results
+mkdir results_uma_instancia
 
-for ((i=1; i<=$ITERATIONS_SIZE; i++)); do
-    THREADS=$INITIAL_THREADS
-    for ((j=1; j<=$ITERATIONS_THREAD; j++)); do
-        for ((k = 1; k<=MEASUREMENTS; k++)); do
-            (mpirun -host $@ ./mandelbrot -2.500  1.500 -2.000 2.000 $THREADS $IMG_SIZE) >> full.log 2>&1
-            (mpirun -host $@ ./mandelbrot -0.800 -0.700  0.050 0.150 $THREADS $IMG_SIZE) >> seahorse.log 2>&1
-            (mpirun -host $@ ./mandelbrot  0.175  0.375 -0.100 0.100 $THREADS $IMG_SIZE) >> elephant.log 2>&1
-            (mpirun -host $@ ./mandelbrot -0.188 -0.012  0.554 0.754 $THREADS $IMG_SIZE) >> triplesp.log 2>&1
-        done
-        THREADS=$(($THREADS * 2))
-    done
-    IMG_SIZE=$(($IMG_SIZE * 2))
+for ((k = 1; k<=MEASUREMENTS; k++)); do
+	perf stat -d mpirun -host $INSTANCES ./mandelbrot -2.500  1.500 -2.000 2.000 $THREADS $SIZE >> full.log 2>&1
+	perf stat -d mpirun -host $INSTANCES ./mandelbrot -0.800 -0.700  0.050 0.150 $THREADS $SIZE >> seahorse.log 2>&1
+	perf stat -d mpirun -host $INSTANCES ./mandelbrot  0.175  0.375 -0.100 0.100 $THREADS $SIZE >> elephant.log 2>&1
+	perf stat -d mpirun -host $INSTANCES ./mandelbrot -0.188 -0.012  0.554 0.754 $THREADS $SIZE >> triplesp.log 2>&1
 done
 
-mv *.log results
+#for ((i=1; i<=$ITERATIONS_SIZE; i++)); do
+#    THREADS=$INITIAL_THREADS
+#    for ((j=1; j<=$ITERATIONS_THREAD; j++)); do
+#        for ((k = 1; k<=MEASUREMENTS; k++)); do
+#            (mpirun -host $@ ./mandelbrot -2.500  1.500 -2.000 2.000 $THREADS $IMG_SIZE) >> full.log 2>&1
+#            (mpirun -host $@ ./mandelbrot -0.800 -0.700  0.050 0.150 $THREADS $IMG_SIZE) >> seahorse.log 2>&1
+#            (mpirun -host $@ ./mandelbrot  0.175  0.375 -0.100 0.100 $THREADS $IMG_SIZE) >> elephant.log 2>&1
+#            (mpirun -host $@ ./mandelbrot -0.188 -0.012  0.554 0.754 $THREADS $IMG_SIZE) >> triplesp.log 2>&1
+#        done
+#        THREADS=$(($THREADS * 2))
+#    done
+#    IMG_SIZE=$(($IMG_SIZE * 2))
+#done
+
+mv *.log results_uma_instancia
 rm output.ppm
