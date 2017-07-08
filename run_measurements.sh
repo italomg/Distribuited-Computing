7ca17180b5fa011385a3c783f69dabf5001f62c6
fernandohlb <fernandohlb@gmail.com>
2017-07-08 18:32:45 +0000

    Ajuste execucao duas insntancias

--- run_measurements.sh
+++ run_measurements.sh
@@ -4,8 +4,9 @@ set -o xtrace
 
 MEASUREMENTS=10
 SIZE=8192
-INSTANCES=localhost
-THREADS=$(nproc)
+INSTANCES=instance-1,instance-2
+THREADS=8
+#$(nproc)
 #ITERATIONS_THREAD=2
 #ITERATIONS_SIZE=2
 #INITIAL_THREADS=$(nproc)
@@ -15,20 +16,7 @@ make
 mkdir results
 
 #MANDELBROT: Execução com uma instância com 8 cores
-mkdir results/uma_instancia
-
-for ((k = 1; k<=MEASUREMENTS; k++)); do
-	perf stat -d mpirun -host $INSTANCES ./mandelbrot -2.500  1.500 -2.000 2.000 $THREADS $SIZE >> full.log 2>&1
-	perf stat -d mpirun -host $INSTANCES ./mandelbrot -0.800 -0.700  0.050 0.150 $THREADS $SIZE >> seahorse.log 2>&1
-	perf stat -d mpirun -host $INSTANCES ./mandelbrot  0.175  0.375 -0.100 0.100 $THREADS $SIZE >> elephant.log 2>&1
-	perf stat -d mpirun -host $INSTANCES ./mandelbrot -0.188 -0.012  0.554 0.754 $THREADS $SIZE >> triplesp.log 2>&1
-done
-
-mv *.log results/uma_instancia
-rm output.ppm
-
-#MANDELBROT: Execução com duas instâncias com 4 cores
-#mkdir results/duas_instancias
+#mkdir results/uma_instancia
 
 #for ((k = 1; k<=MEASUREMENTS; k++)); do
 #	perf stat -d mpirun -host $INSTANCES ./mandelbrot -2.500  1.500 -2.000 2.000 $THREADS $SIZE >> full.log 2>&1
@@ -37,8 +25,21 @@ rm output.ppm
 #	perf stat -d mpirun -host $INSTANCES ./mandelbrot -0.188 -0.012  0.554 0.754 $THREADS $SIZE >> triplesp.log 2>&1
 #done
 
-#mv *.log results/duas_instancias
+#mv *.log results/uma_instancia
 #rm output.ppm
+
+#MANDELBROT: Execução com duas instâncias com 4 cores
+mkdir results/duas_instancias
+
+for ((k = 1; k<=MEASUREMENTS; k++)); do
+	perf stat -d mpirun -host $INSTANCES ./mandelbrot -2.500  1.500 -2.000 2.000 $THREADS $SIZE >> full.log 2>&1
+	perf stat -d mpirun -host $INSTANCES ./mandelbrot -0.800 -0.700  0.050 0.150 $THREADS $SIZE >> seahorse.log 2>&1
+	perf stat -d mpirun -host $INSTANCES ./mandelbrot  0.175  0.375 -0.100 0.100 $THREADS $SIZE >> elephant.log 2>&1
+	perf stat -d mpirun -host $INSTANCES ./mandelbrot -0.188 -0.012  0.554 0.754 $THREADS $SIZE >> triplesp.log 2>&1
+done
+
+mv *.log results/duas_instancias
+rm output.ppm
 
 
 
