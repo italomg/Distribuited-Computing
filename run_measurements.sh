f63cbfbd98a101a7f50921b46727c0847b3b7e3e
fernandohlb <fernandohlb@gmail.com>
2017-07-09 00:52:48 +0000

    resultados com uma instancia

--- run_measurements.sh
+++ run_measurements.sh
@@ -2,9 +2,9 @@
 
 set -o xtrace
 
-MEASUREMENTS=1
+MEASUREMENTS=10
 SIZE=8192
-INSTANCES=instance-1,instance-2
+INSTANCES=localhost
 THREADS=$(nproc)
 
 make
@@ -24,14 +24,14 @@ mkdir results
 #rm output.ppm
 
 #MANDELBROT: Execução com duas instâncias com 4 cores
-mkdir results/duas_instancias
+mkdir results/uma_instancia
 
 perf stat -r $MEASUREMENTS -d mpirun -host $INSTANCES ./mandelbrot -2.500  1.500 -2.000 2.000 $THREADS $SIZE >> full.log 2>&1
 perf stat -r $MEASUREMENTS -d mpirun -host $INSTANCES ./mandelbrot -0.800 -0.700  0.050 0.150 $THREADS $SIZE >> seahorse.log 2>&1
 perf stat -r $MEASUREMENTS -d mpirun -host $INSTANCES ./mandelbrot  0.175  0.375 -0.100 0.100 $THREADS $SIZE >> elephant.log 2>&1
 perf stat -r $MEASUREMENTS -d mpirun -host $INSTANCES ./mandelbrot -0.188 -0.012  0.554 0.754 $THREADS $SIZE >> triplesp.log 2>&1
 
-mv *.log results/duas_instancias
+mv *.log results/uma_instancia
 rm output.ppm
 
 
