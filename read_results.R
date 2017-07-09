e6a9f48d5d564fdc80cc1cf93932f7b4eccbccee
Fernando <nando_hlb@yahoo.com.br>
2017-07-08 23:22:29 -0300

    Geração dos Resultados

--- read_results.R
+++ read_results.R
@@ -1,40 +1,59 @@
 library("ggplot2")
 
-full = read.csv("./results/full.log", header = F, stringsAsFactors = F)
-elephant = read.csv("./results/elephant.log", header = F, stringsAsFactors = F)
-seahorse = read.csv("./results/seahorse.log", header = F, stringsAsFactors = F)
-triplesp = read.csv("./results/triplesp.log", header = F, stringsAsFactors = F)
+full_uma_instancia = read.table("./results/uma_instancia/full.log", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+elephant_uma_instancia = read.table("./results/uma_instancia/elephant.log", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+seahorse_uma_instancia = read.table("./results/uma_instancia/seahorse.log", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+triplesp_uma_instancia = read.table("./results/uma_instancia/triplesp.log", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+
+full_duas_instancias = read.table("./results/duas_instancias/full.log", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+elephant_duas_instancias = read.table("./results/duas_instancias/elephant.log", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+seahorse_duas_instancias = read.table("./results/duas_instancias/seahorse.log", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+triplesp_duas_instancias = read.table("./results/duas_instancias/triplesp.log", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+
 
 fulldata = rbind(
-  full,
-  elephant,
-  seahorse,
-  triplesp
+  full_uma_instancia,
+  elephant_uma_instancia,
+  seahorse_uma_instancia,
+  triplesp_uma_instancia,
+  full_duas_instancias,
+  elephant_duas_instancias,
+  seahorse_duas_instancias,
+  triplesp_duas_instancias
 )
 
 names(fulldata) <- "output_line" 
+results <- split(fulldata, f = rep(1:(nrow(fulldata)/16), each = 16))
 
-results <- split(fulldata, f = rep(1:(nrow(fulldata)/10), each = 10))
 
 read_result <- function(df) {
   
   output <- lapply(strsplit(df$output_line, "[ :]"), function(x) x[x != ""])
-  num_threads <- as.numeric(gsub(",", ".",output[[2]][8]))
-  img_size <- as.numeric(gsub(",", ".",output[[2]][6]))
-  time_allocation <- as.numeric(gsub(",", ".",output[[3]][4]))
-  time_algorithm <- as.numeric(gsub(",", ".",output[[5]][4]))
-  time_ioops <- as.numeric(gsub(",", ".",output[[7]][5]))
-  time_elapsed <- as.numeric(gsub(",", ".",output[[9]][4]))
- 
-  results <- data.frame(
-    num_hosts = 1, # Change manually according to GCP setup
-    num_threads = num_threads,
-    img_size = img_size,
-    time_allocation = time_allocation,
-    time_algorithm = time_algorithm,
-    time_ioops = time_ioops,
-    time_elapsed = time_elapsed
-  )
+
+  instancias <- unlist(strsplit(output[[1]][7],split=","))
+  qtdInstancias = length(instancias)
+  
+  image <- switch(output[[1]][9],
+              "-2.500" = {"full"},
+              "-0.800" = {"seahorse"},
+              "0.175"  = {"elephant"},
+               "-0.188" = {"triple_spiral"},
+              stop("Image unrecognized.")) 
+   nthreads <- as.numeric(output[[1]][13])
+   imsize <- as.numeric(output[[1]][14])
+   time_elapsed <- as.numeric(gsub(",", "", output[[16]][1]))
+   per_stddev <- as.numeric(gsub("%", "",output[[16]][7]))/100
+   val_stddev = time_elapsed * per_stddev
+   
+   results <- data.frame(
+     qtdInstancias = qtdInstancias, # Change manually according to GCP setup
+     image = image,
+     nthreads = nthreads,
+     imsize = imsize,
+     time_elapsed = time_elapsed,
+     per_stddev = per_stddev,
+     val_stddev = val_stddev
+   )
   
 }
 
@@ -47,53 +66,15 @@ fulldata = rbind(
 
 write.csv(fulldata, file = "./results/results.csv")
 
- fig1 <- ggplot(data = fulldata, aes(x = as.factor(img_size), y = time_elapsed, color = type)) +
-   geom_boxplot() +
-#   facet_wrap(~imsize, scales = "free", labeller = "label_both") +
-   ggtitle("Tempo de Execução x Tamanho da Imagem em Pixels") +
-   xlab("Tamanho da Imagem em Pixels") +
-   ylab("Tempo de execução (s)") +
-   guides(color = guide_legend(title="Implementação")) +
-   theme(plot.title = element_text(hjust = 0.5, vjust = 5, size = 20),
-         strip.text = element_text(size= 16),
-         axis.title = element_text(size = 16),
-         legend.title = element_text(size = 16),
-         legend.text = element_text(size = 16),
-         axis.text = element_text(size = 12),
-         axis.title.y=element_text(vjust=5),
-         axis.title.x=element_text(vjust=-10),
-         plot.margin = unit(c(1,1,1,1), "cm"))
-
-pdf("resultado.pdf", width = 16, height = 9)
-dev.off()
-
-dados <- read.csv("./results/results.csv")
-library("dplyr")
-
-select(dados, num_hosts, num_threads, img_size, time_elapsed, time_allocation, time_algorithm, time_ioops)
-
-grouped_result <- group_by(dados, num_hosts, num_threads, img_size)
-
-summarised_result <- summarise(grouped_result,
-                               media_time_elapsed = mean(time_elapsed, na.rm = TRUE),
-                               desv_pad_time_elapsed = sd(time_elapsed),
-                               media_time_allocation = mean(time_allocation, na.rm = TRUE),
-                               desv_pad_time_allocation = sd(time_allocation),
-                               media_time_algorithm = mean(time_algorithm, na.rm = TRUE),
-                               desv_pad_time_algorithm = sd(time_algorithm),
-                               media_time_ioops = mean(time_ioops, na.rm = TRUE),
-                               desv_pad_time_ioops = sd(time_ioops))
-
-
-write.csv(summarised_result, file = "./results/summarised_result.csv")  
-
-fig2 <- ggplot(data=summarised_result, aes(x = factor(img_size), y=media_time_algorithm, fill=type)) +
-  geom_bar(stat="identity", position=position_dodge())+
-  #   facet_wrap(~imsize, scales = "free", labeller = "label_both") +
-  ggtitle("Média do Tempo de Execução x Tamanho da Imagem Pixels") +
-  xlab("Tamanho da Imagem Pixels") +
-  ylab("Tempo de execução (s)") +
-  guides(color = guide_legend(title="Implementação")) +
+fig1 <- ggplot(data = fulldata, aes(x = as.factor(qtdInstancias), y = time_elapsed, color = image,group = image)) +
+  geom_errorbar(aes(ymin=time_elapsed-val_stddev, ymax=time_elapsed+val_stddev), width=.1) +
+  geom_line() +
+  geom_point()+
+  ggtitle("Média do Tempo de Execução x Quantidade de Instâncias",
+          subtitle = "Média com Desvio Padrão") +
+  xlab("Quantidade de Instâncias") +
+  ylab("Média do Tempo de execução (s)") +
+  guides(color = guide_legend(title="Região Processada")) +
   theme(plot.title = element_text(hjust = 0.5, vjust = 5, size = 20),
         strip.text = element_text(size= 16),
         axis.title = element_text(size = 16),
@@ -104,9 +85,53 @@ fig2 <- ggplot(data=summarised_result, aes(x = factor(img_size), y=media_time_al
         axis.title.x=element_text(vjust=-10),
         plot.margin = unit(c(1,1,1,1), "cm"))
 
-pdf("resultado_bar.pdf", width = 16, height = 9)
-print(fig2)
-dev.off() 
+pdf("resultados.pdf", width = 16, height = 9)
+print(fig1)
+dev.off()
+
+
+
+
+#dados <- read.csv("./results/results.csv")
+#library("dplyr")
+
+#select(dados, num_hosts, num_threads, img_size, time_elapsed, time_allocation, time_algorithm, time_ioops)
+
+#grouped_result <- group_by(dados, num_hosts, num_threads, img_size)
+
+#summarised_result <- summarise(grouped_result,
+#                               media_time_elapsed = mean(time_elapsed, na.rm = TRUE),
+#                               desv_pad_time_elapsed = sd(time_elapsed),
+#                               media_time_allocation = mean(time_allocation, na.rm = TRUE),
+#                               desv_pad_time_allocation = sd(time_allocation),
+#                               media_time_algorithm = mean(time_algorithm, na.rm = TRUE),
+#                               desv_pad_time_algorithm = sd(time_algorithm),
+#                               media_time_ioops = mean(time_ioops, na.rm = TRUE),
+#                               desv_pad_time_ioops = sd(time_ioops))
+
+
+#write.csv(summarised_result, file = "./results/summarised_result.csv")  
+
+#fig2 <- ggplot(data=summarised_result, aes(x = factor(img_size), y=media_time_algorithm, fill=type)) +
+#  geom_bar(stat="identity", position=position_dodge())+
+  #   facet_wrap(~imsize, scales = "free", labeller = "label_both") +
+#  ggtitle("Média do Tempo de Execução x Tamanho da Imagem Pixels") +
+#  xlab("Tamanho da Imagem Pixels") +
+#  ylab("Tempo de execução (s)") +
+#  guides(color = guide_legend(title="Implementação")) +
+#  theme(plot.title = element_text(hjust = 0.5, vjust = 5, size = 20),
+#        strip.text = element_text(size= 16),
+#        axis.title = element_text(size = 16),
+#        legend.title = element_text(size = 16),
+#        legend.text = element_text(size = 16),
+#        axis.text = element_text(size = 12),
+#        axis.title.y=element_text(vjust=5),
+#        axis.title.x=element_text(vjust=-10),
+#        plot.margin = unit(c(1,1,1,1), "cm"))
+
+#pdf("resultado_bar.pdf", width = 16, height = 9)
+#print(fig2)
+#dev.off() 
 
  ######################################
 
